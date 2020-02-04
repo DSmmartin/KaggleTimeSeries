3e57ea068b962ad8c727a97ab3f183f8671691c3
manolin.martin <mjmm_manuel@hotmail.com>
2020-02-04 18:10:35 +0100

    add pylint and solve

--- project/kaggle_web_traffic_forecasting/spark_manage/spark_manage.py
+++ project/kaggle_web_traffic_forecasting/spark_manage/spark_manage.py
@@ -1,8 +1,21 @@
-import pyspark
+""" Spark manager instance
+"""
+
 import os
 import sys
+import pyspark # pylint:  disable=E0401
+
 
 def spark_start(show_output=False):
+    """ Spark initialization instance
+
+    Keyword Arguments:
+        show_output {bool} -- show environment configuration (default: {False})
+
+    Returns:
+        SparkSession -- spark application
+    """
+
     print("Starting Spark")
     spark = pyspark.sql.SparkSession.builder.getOrCreate()
 
@@ -15,8 +28,14 @@ def spark_start(show_output=False):
     return spark
 
 def print_environment(spark):
+    """ show environment configuration
+
+    Arguments:
+        spark {SparkSession} -- spark application
+    """
+
     print("Work directory:", os.getcwd())
-    print ('****************')
-    print ('Python version: {}'.format(sys.version))
-    print ('Spark version: {}'.format(spark.version))
-    print ('****************')
+    print('****************')
+    print('Python version: {}'.format(sys.version))
+    print('Spark version: {}'.format(spark.version))
+    print('****************')
